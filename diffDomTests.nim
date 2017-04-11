b22567d9e0d037b7d67ad03e7ba80d91b1e3be9b
Andreas Rumpf <rumpf_a@web.de>
2017-04-11 16:55:51 +0200

    cleanup of the tests

--- diffDomTests.nim
+++ diffDomTests.nim
@@ -3,15 +3,16 @@ import vdom, times, karax, karaxdsl, jdict, jstrutils, parseutils, sequtils
 
 var
     entries: seq[cstring]
+    results: seq[cstring]
     timeout : Timeout
 
 proc reset() =
-    kout cstring"reset started"
+    results.add cstring"reset started"
     entries = @[cstring("0"), cstring("1"), cstring("2"), cstring("3"), cstring("4"), cstring("5") ]
     redraw()
-    kout cstring"reset finished"
+    results.add cstring"reset finished"
 
-proc checkOrder(order : seq[int]): bool = 
+proc checkOrder(order : seq[int]): bool =
     var ul = getElementById("ul")
     if ul == nil or len(ul.children) != len(order):
         return false
@@ -25,13 +26,13 @@ proc checkOrder(order : seq[int]): bool =
 proc check1() =
     let result = checkOrder(@[0, 1, 2, 3, 4, 7, 5])
     if result:
-        kout cstring"test1 - OK"
+        results.add cstring"test1 - OK"
     else:
-        kout cstring"test1 - FAIL"
+        results.add cstring"test1 - FAIL"
 
 # result: 0 1 2 3 4 7 5
 proc test1() =
-    kout cstring"test1 started"
+    results.add cstring"test1 started"
     entries = @[cstring("0"), cstring("1"), cstring("2"), cstring("3"), cstring("4"), cstring("5") ]
     entries.insert(cstring("7"), 5)
     redraw()
@@ -40,13 +41,13 @@ proc test1() =
 proc check2() =
     let result = checkOrder(@[8, 0, 1, 2, 3, 4, 7, 5])
     if result:
-        kout cstring"test1 - OK"
+        results.add cstring"test1 - OK"
     else:
-        kout cstring"test2 - FAIL"
+        results.add cstring"test2 - FAIL"
 
 # result: 8 0 1 2 3 4 7 5
 proc test2() =
-    kout cstring"test2 started"
+    results.add cstring"test2 started"
     entries = @[cstring("0"), cstring("1"), cstring("2"), cstring("3"), cstring("4"), cstring("5") ]
     entries.insert(cstring("7"), 5)
     entries.insert(cstring("8"), 0)
@@ -56,13 +57,13 @@ proc test2() =
 proc check3() =
     let result = checkOrder(@[2, 3, 4, 1])
     if result:
-        kout cstring"test3 - OK"
+        results.add cstring"test3 - OK"
     else:
-        kout cstring"test3 - FAIL"
+        results.add cstring"test3 - FAIL"
 
 # result: 2 3 4 1
 proc test3() =
-    kout cstring"test3 started"
+    results.add cstring"test3 started"
     entries = @[cstring("2"), cstring("3"), cstring("4"), cstring("1") ]
     redraw()
     timeout = setTimeout(check3, 20)
@@ -70,13 +71,13 @@ proc test3() =
 proc check4() =
     let result = checkOrder(@[5, 6, 7, 8])
     if result:
-        kout cstring"test4 - OK"
+        results.add cstring"test4 - OK"
     else:
-        kout cstring"test4 - FAIL"
+        results.add cstring"test4 - FAIL"
 
 # result: 5 6 7 8
 proc test4() =
-    kout cstring"test4 started"
+    results.add cstring"test4 started"
     entries = @[cstring("5"), cstring("6"), cstring("7"), cstring("8") ]
     redraw()
     timeout = setTimeout(check4, 20)
@@ -84,27 +85,27 @@ proc test4() =
 proc check5() =
     let result = checkOrder(@[0, 1, 3, 5, 4, 5])
     if result:
-        kout cstring"test5 - OK"
+        results.add cstring"test5 - OK"
     else:
-        kout cstring"test5 - FAIL"
+        results.add cstring"test5 - FAIL"
 
 # result: 0 1 3 5 4 5
 proc test5() =
-    kout cstring"test5 started"
+    results.add cstring"test5 started"
     entries = @[cstring("0"), cstring("1"), cstring("3"), cstring("5"), cstring("4"), cstring("5") ]
     redraw()
     timeout = setTimeout(check5, 20)
-     
+
 proc check6() =
     let result = checkOrder(@[])
     if result:
-        kout cstring"test6 - OK"
+        results.add cstring"test6 - OK"
     else:
-        kout cstring"test6 - FAIL"
+        results.add cstring"test6 - FAIL"
 
 # result: empty
 proc test6() =
-    kout cstring"test6 started"
+    results.add cstring"test6 started"
     entries = @[]
     redraw()
     timeout = setTimeout(check6, 20)
@@ -112,17 +113,18 @@ proc test6() =
 proc check7() =
     let result = checkOrder(@[2])
     if result:
-        kout cstring"test7 - OK"
+        results.add cstring"test7 - OK"
     else:
-        kout cstring"test7 - FAIL"
+        results.add cstring"test7 - FAIL"
+    redraw()
 
 # result: 2
 proc test7() =
-    kout cstring"test7 started"
+    results.add cstring"test7 started"
     entries = @[cstring("2")]
     redraw()
     timeout = setTimeout(check7, 20)
-    
+
 proc createEntry(id: int): VNode =
   result = buildHtml():
     button(id="" & $id):
@@ -132,8 +134,11 @@ proc createDom(): VNode =
     result = buildHtml(tdiv()):
         ul(id="ul"):
             for e in entries:
-                createEntry(parseInt(e)) 
-                
+                createEntry(parseInt(e))
+        for r in results:
+            tdiv:
+                text r
+
 setRenderer createDom
 
 proc onload(session: cstring) {.exportc.} =
