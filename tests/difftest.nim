bb1732c834454838dab0747389d162e7230c2eac
Andreas Rumpf <rumpf_a@web.de>
2017-10-02 16:00:30 +0200

    make tests green again

--- tests/difftest.nim
+++ tests/difftest.nim
@@ -165,7 +165,8 @@ proc testWildInsert() =
   let b = createEntries(entries)
   doDiff(a, b, "pkInsertBefore button 7", "pkAppend div 5")
 
-kxi = KaraxInstance(rootId: cstring"ROOT", renderer: proc (): VNode = discard)
+kxi = KaraxInstance(rootId: cstring"ROOT", renderer: proc (): VNode = discard,
+                    byId: newJDict[cstring, VNode]())
 
 testAppend()
 testInsert()
