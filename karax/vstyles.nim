1a4f5b3fa029581447d07d3341d32b177a68f0bd
gogolxdong <lxdong.007@gmail.com>
2019-07-11 14:43:53 +0800

    fix tailing ;

--- karax/vstyles.nim
+++ karax/vstyles.nim
@@ -270,16 +270,16 @@ proc getAttr*(s: VStyle; attr: StyleAttr): kstring {.noSideEffect.} =
       return ""
     inc i, 2
 
-proc style*(pairs: string):VStyle {.noSideEffect.} =
+proc style*(pairs: kstring):VStyle {.noSideEffect.} =
     ## constructs a VStyle object from string literal.
     when defined(js):
       result = newJSeq[cstring]()
     else:
       new(result)
       result[] = @[]
-    for styles in split(pairs, ";"):
-      var x = split(styles, ":")
-      if x.len == 2:
+    for s in split($pairs, ";"):
+      if ":" in s:
+        var x = split(s, ":")
         result.setAttr strip(x[0]), strip(x[1])
 
 proc style*(pairs: varargs[(StyleAttr, kstring)]): VStyle {.noSideEffect.} =
