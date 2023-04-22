24c64a929ec22592a8cdc6391632d41bd0775049
scattergory <111326462+scattergory@users.noreply.github.com>
2023-04-22 19:59:50 -0400

    Update vdom.nim
    make namespace declarations user-modifiable

--- karax/vdom.nim
+++ karax/vdom.nim
@@ -86,8 +86,9 @@ const
     munderover, semantics}
     
 var 
-  svgNamespace = "http://www.w3.org/2000/svg"
-  mathNamespace = "http://www.w3.org/1998/Math/MathML"
+  svgNamespace* = "http://www.w3.org/2000/svg"
+  mathNamespace* = "http://www.w3.org/1998/Math/MathML"
+  #global var settings: defaults are already set up, but may be adjusted in one shot for the user's entire project
 
 type
   EventKind* {.pure.} = enum ## The events supported by the virtual DOM.
