644858b7e77a790a34116b3491ade30e069a4aa8
scattergory <111326462+scattergory@users.noreply.github.com>
2023-04-26 12:03:22 -0400

    Update vdom.nim

--- karax/vdom.nim
+++ karax/vdom.nim
@@ -31,7 +31,7 @@ type
     a, em, strong, small,
     strikethrough = "s", cite, quote,
     dfn, abbr, data, time, code, `var` = "var", samp,
-    kdb, sub, sup, italic = "i", bold = "b", underlined = "u",
+    kbd, sub, sup, italic = "i", bold = "b", underlined = "u",
     mark, ruby, rt, rp, bdi, dbo, span, br, wbr,
     ins, del, img, iframe, embed, `object` = "object",
     param, video, audio, source, track, canvas, map, area,
